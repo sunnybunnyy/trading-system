b43f5039d8bf4097efd6532822c3a497a0a1cd6d
Radha <kradha6113@gmail.com>
2025-12-16 20:15:10 -0500

    Add MarketUpdate struct

---
+++ src/events.cpp
@@ -0,0 +1,16 @@
+module;
+
+#include <chrono>
+
+export module events;
+
+using time_point = std::chrono::time_point<std::chrono::steady_clock>;
+
+export struct MarketUpdate {
+    int symbol_id;
+    double bid_price;
+    double ask_price;
+    int bid_size;
+    int ask_size;
+    time_point timestamp = std::chrono::steady_clock::now();
+};
