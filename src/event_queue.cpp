2aa21b0eb78c11937d5015a624006c48c76d113b
Radha <kradha6113@gmail.com>
2025-12-18 17:38:10 -0500

    Add EventQueue class

---
+++ src/event_queue.cpp
@@ -0,0 +1,35 @@
+module;
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+export module EventQueue;
+
+export template <typename T>
+class EventQueue {
+    public:
+        void push(const T& item) {
+            {
+                std::lock_guard<std::mutex> lock(mutex);
+                queue.push(item);
+            }
+            cv.notify_one();
+        }
+
+        T pop() {
+            std::unique_lock<std::mutex> lock(mutex);
+            cv.wait(lock, [this]() { 
+                return !queue.empty();
+            });
+
+            T item = queue.front();
+            queue.pop();
+            return item;
+        }
+
+    private:
+        std::queue<T> queue;
+        std::mutex mutex;
+        std::condition_variable cv;
+};
