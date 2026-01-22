7f5f933bf5af441eeefc51afd37434154276b6ab
Radha <kradha6113@gmail.com>
2026-01-22 06:54:20 -0500

    Add on_price() to Momentum class

--- strategies/momentum.py
+++ strategies/momentum.py
@@ -5,3 +5,21 @@ class Momentum:
         self.fast_ema = None
         self.slow_ema = None
         self.position = 0
+
+    def on_price(self, price):
+        if self.fast_ema is None:
+            self.fast_ema = price
+            self.slow_ema = price
+            return 0
+        
+        self.fast_ema = self.alpha_fast * price + (1 - self.alpha_fast) * self.fast_ema
+        self.slow_ema = self.alpha_slow * price + (1 - self.alpha_slow) * self.slow_ema
+
+        if self.fast_ema > self.slow_ema and self.position <= 0:
+            self.position = 1
+            return 1
+        elif self.fast_ema < self.slow_ema and self.position >= 0:
+            self.position = -1
+            return -1
+        
+        return 0
