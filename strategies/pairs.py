bdfc2a440a375960ad33f253f71bf24274ebcb92
Radha <kradha6113@gmail.com>
2026-01-25 11:18:59 -0500

    Implement ring buffer for Pairs tick lists

--- strategies/pairs.py
+++ strategies/pairs.py
@@ -8,3 +8,12 @@ class Pairs:
         self.index = 0
         self.count = 0
         self.position = 0
+
+    def on_prices(self, price_x, price_y):
+        self.x[self.index] = price_x
+        self.y[self.index] = price_y
+        self.index = (self.index + 1) % self.window
+        self.count = min(self.count + 1, self.window)
+
+        if self.count < self.window:
+            return 0
